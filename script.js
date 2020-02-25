9c6abd1abce772825a651dc49352f870b0c0af10
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-02-25 20:46:14 +0100

    Basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const idCol = document.querySelector('#id')
+const titleCol = document.querySelector('#title')
+const authorCol = document.querySelector('#author')
+const pagesCol = document.querySelector('#pages')
+const readCol = document.querySelector('#read')
+const changeCol = document.querySelector('#change')
+const removeCol = document.querySelector('#remove')
+
+const newBook = document.querySelector('#newBook')
+newBook.addEventListener('click', () => addBookToLibrary())
+
 let myLibrary = [];
 
-function book(title, author, pages, read) {
+function book(id, title, author, pages, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -11,32 +23,44 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // const title = document.createElement('input')
-  // document.body.appendChild(title)
-  // const author = document.createElement('input')
-  // document.body.appendChild(author)
-  // const pages = document.createElement('input')
-  // document.body.appendChild(pages)
-  // const read = document.createElement('input')
-  // read.setAttribute('type', 'checkbox')
-  // document.body.appendChild(read)
+  const id = myLibrary.length + 1
   const title = prompt('Enter the book title:')
   const author = prompt('Enter the author:')
-  const pages = prompt('Enter the number of pages:')
+  const pages = parseInt(prompt('Enter the number of pages:'))
   const read = prompt('Enter read/not read:')
 
-  const newBook = new book(title, author, pages, read);
+  const newBook = new book(id, title, author, pages, read);
   myLibrary.push(newBook)
   console.log(myLibrary)
+  render()
 }
 
 function render() {
-
+  let book = myLibrary[myLibrary.length - 1]
+  let id = document.createElement('div')
+  id.innerHTML = book.id
+  idCol.appendChild(id)
+  let title = document.createElement('div')
+  title.innerHTML = book.title
+  titleCol.appendChild(title)
+  let author = document.createElement('div')
+  author.innerHTML = book.author
+  authorCol.appendChild(author)
+  let pages = document.createElement('div')
+  pages.innerHTML = book.pages
+  pagesCol.appendChild(pages)
+  let read = document.createElement('div')
+  read.innerHTML = book.read
+  readCol.appendChild(read)
+  let change = document.createElement('input')
+  change.type = 'checkbox'
+  changeCol.appendChild(change)
+  let remove = document.createElement('button')
+  remove.innerHTML = 'Remove'
+  removeCol.appendChild(remove)
 }
 
 // const theHobbit = new book('The Hobbit', 'Tolkien', 295, 'not read')
 
 // console.log(theHobbit.info());
 
-const newBook = document.querySelector('#newBook')
-newBook.addEventListener('click', () => addBookToLibrary())
